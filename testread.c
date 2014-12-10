1ff3114063261e88c82805aa96ad8fc636c773fb
nmartin <nmartin867@gmail.com>
2014-12-09 20:11:15 -0700

    Pin start sig

--- testread.c
+++ testread.c
@@ -8,17 +8,58 @@
 int dht_dat[5] = {0, 0, 0, 0, 0};
 
 void read_dht11_dat(){
- uint8_t laststate = HIGH;
- uint8_t counter   = 0;
- uint8_t j         = 0,i;
- float   f;
+	uint8_t laststate = HIGH;
+	uint8_t counter   = 0;
+	uint8_t j         = 0,i;
+	float   f;
+
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+	/* pull pin down for 18 milliseconds */
+	pinMode( DHTPIN, OUTPUT );
+	digitalWrite( DHTPIN, LOW );
+	delay( 18 );
+	/* then pull it up for 40 microseconds */
+	digitalWrite( DHTPIN, HIGH );
+	delayMicroseconds( 40 );
+	/* prepare to read the pin */
+	pinMode( DHTPIN, INPUT );
+
+	/* detect change and read data */
+	for ( i = 0; i < MAXTIMINGS; i++ )
+	{
+		counter = 0;
+		while ( digitalRead( DHTPIN ) == laststate )
+		{
+			counter++;
+			delayMicroseconds( 1 );
+			if ( counter == 255 )
+			{
+				break;
+			}
+		}
+		laststate = digitalRead( DHTPIN );
+		
+		if ( counter == 255 )
+			break;
+		
+		/* ignore first 3 transitions */
+		if ( (i >= 4) && (i % 2 == 0) )
+		{
+			/* shove each bit into the storage bytes */
+			dht11_dat[j / 8] <<= 1;
+			if ( counter > 16 )
+				dht11_dat[j / 8] |= 1;
+			j++;
+		}
+	}
 }
 
 int main(void){
- printf("Read from DHT11 sensor.\n");
- 
- if(wiringPiSetup() == -1)
-  exit(1);
+	printf("Read from DHT11 sensor.\n");
 
-  return(0);
+	if(wiringPiSetup() == -1)
+		exit(1);
+
+	return(0);
 }
